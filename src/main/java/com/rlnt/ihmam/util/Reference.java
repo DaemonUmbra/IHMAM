a8f29a037c579fb563d5a2349451b88b17d4752a
Relentless <damnrelentless.yt@gmail.com>
2019-10-03 03:12:34 +0200

    Added reference file

---
+++ src/main/java/com/rlnt/ihmam/util/Reference.java
@@ -0,0 +1,13 @@
+package com.rlnt.ihmam.util;
+
+public class Reference {
+    // Basic Information
+    public static final String MOD_ID = "ihmam";
+    public static final String MOD_NAME = "I helped making a mistake";
+    public static final String MOD_VERSION = "@VERSION@";
+    public static final String MC_VERSION = "[@MCVERSION@]";
+
+    // Proxies
+    public static final String CLIENT = "com.rlnt.ihmam.proxy.client.ClientProxy";
+    public static final String SERVER = "com.rlnt.ihmam.proxy.common.CommonProxy";
+}
