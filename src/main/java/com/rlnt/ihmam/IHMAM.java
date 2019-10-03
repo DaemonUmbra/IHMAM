71ec33d002bb3f5a9403ce8dab8814af6c752e1a
Relentless <damnrelentless.yt@gmail.com>
2019-10-03 03:13:16 +0200

    Make main class use reference file

--- src/main/java/com/rlnt/ihmam/IHMAM.java
+++ src/main/java/com/rlnt/ihmam/IHMAM.java
@@ -1,35 +1,29 @@
 package com.rlnt.ihmam;
 
 import com.rlnt.ihmam.proxy.IProxy;
+import com.rlnt.ihmam.util.Reference;
 import net.minecraftforge.common.config.Configuration;
 import net.minecraftforge.fml.common.Mod;
 import net.minecraftforge.fml.common.SidedProxy;
 import org.apache.logging.log4j.Logger;
 
 @Mod(
-        modid = IHMAM.MOD_ID,
-        name = IHMAM.MOD_NAME,
-        version = IHMAM.MOD_VERSION,
-        acceptedMinecraftVersions = IHMAM.MC_VERSION
+        modid = Reference.MOD_ID,
+        name = Reference.MOD_NAME,
+        version = Reference.MOD_VERSION,
+        acceptedMinecraftVersions = Reference.MC_VERSION
 )
 public class IHMAM {
-    public static final String MOD_ID = "ihmam";
-    public static final String MOD_NAME = "I helped making a mistake";
-    public static final String MOD_VERSION = "@VERSION@";
-    public static final String MC_VERSION = "[@MCVERSION@]";
 
-    // Proxies
-    public static final String CLIENT = "com.rlnt.ihmam.proxy.client.ClientProxy";
-    public static final String SERVER = "com.rlnt.ihmam.proxy.common.CommonProxy";
-
-    @SidedProxy(clientSide = IHMAM.CLIENT, serverSide = IHMAM.SERVER)
-    public static IProxy proxy;
-
-    @Mod.Instance(MOD_ID)
+    // Instance
+    @Mod.Instance
     public static IHMAM instance;
+
+    // Proxy
+    @SidedProxy(clientSide = Reference.CLIENT, serverSide = Reference.SERVER)
+    public static IProxy proxy;
 
     // Config and Logger
     public static Logger logger;
     public static Configuration config;
-
 }
