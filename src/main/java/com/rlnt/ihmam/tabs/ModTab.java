ce8827e75fec12d4aa3a5a0e62df1815883b4291
Relentless <damnrelentless.yt@gmail.com>
2019-10-06 23:34:57 +0200

    Fixed tab being null

--- src/main/java/com/rlnt/ihmam/tabs/ModTab.java
+++ src/main/java/com/rlnt/ihmam/tabs/ModTab.java
@@ -7,12 +7,15 @@ import net.minecraft.item.ItemStack;
 import net.minecraftforge.fml.relauncher.Side;
 import net.minecraftforge.fml.relauncher.SideOnly;
 
+import javax.annotation.Nonnull;
+
 public class ModTab extends CreativeTabs {
 
     public ModTab(String name) {
         super(Reference.MODID + "." + name);
     }
 
+    @Nonnull
     @SideOnly(Side.CLIENT)
     @Override
     public ItemStack createIcon() {
