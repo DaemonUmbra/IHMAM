110be16d8957d7c92bcbcdb6de7064f8cb8a359a
Relentless <damnrelentless.yt@gmail.com>
2019-10-07 10:42:22 +0200

    More fluid changes

--- src/main/java/com/rlnt/ihmam/IHMAM.java
+++ src/main/java/com/rlnt/ihmam/IHMAM.java
@@ -1,11 +1,15 @@
 package com.rlnt.ihmam;
 
+import com.rlnt.ihmam.util.handlers.RegistryHandler;
 import com.rlnt.ihmam.util.interfaces.IProxy;
 import com.rlnt.ihmam.tabs.ModTab;
 import com.rlnt.ihmam.util.Reference;
 import net.minecraft.creativetab.CreativeTabs;
+import net.minecraftforge.fluids.FluidRegistry;
 import net.minecraftforge.fml.common.Mod;
+import net.minecraftforge.fml.common.Mod.EventHandler;
 import net.minecraftforge.fml.common.SidedProxy;
+import net.minecraftforge.fml.common.event.FMLPreInitializationEvent;
 
 @Mod(
         modid = Reference.MODID,
@@ -21,4 +25,14 @@ public class IHMAM {
 
     // Creative Tab
     public static final CreativeTabs IHMAM_TAB = new ModTab("tabIHMAM");
+
+    // Fluid Registry
+    static {
+        FluidRegistry.enableUniversalBucket();
+    }
+
+    @EventHandler
+    public static void preInit(FMLPreInitializationEvent event) {
+        RegistryHandler.preInitRegistries(event);
+    }
 }
