6834d6efd3d6f52a21662f60f025ff25bf18030e
Relentless <damnrelentless.yt@gmail.com>
2019-10-07 10:39:41 +0200

    Added fluid components

--- src/main/java/com/rlnt/ihmam/init/ModItems.java
+++ src/main/java/com/rlnt/ihmam/init/ModItems.java
@@ -8,10 +8,10 @@ import java.util.List;
 
 public class ModItems {
 
+    // Item List
     public static final List<Item> ITEMS = new ArrayList<Item>();
 
-    // Items
-
+    /*  Items  */
     // Crafting Components
     // Basic
     public static final Item BASIC_CRAFTING_COMPONENT_1 = new ItemBase("itemBasicCraftingComponent1", "basic_crafting_component_1");
@@ -33,4 +33,9 @@ public class ModItems {
     public static final Item RUINED_CRAFTING_COMPONENT_1 = new ItemBase("itemRuinedCraftingComponent1", "ruined_crafting_component_1");
     public static final Item RUINED_CRAFTING_COMPONENT_2 = new ItemBase("itemRuinedCraftingComponent2", "ruined_crafting_component_2");
     public static final Item RUINED_CRAFTING_COMPONENT_3 = new ItemBase("itemRuinedCraftingComponent3", "ruined_crafting_component_3");
+
+    // Fluid Components
+    public static final Item FLUID_COMPONENT_1 = new ItemBase("itemFluidComponent1", "fluid_component_1");
+    public static final Item FLUID_COMPONENT_2 = new ItemBase("itemFluidComponent2", "fluid_component_2");
+    public static final Item FLUID_COMPONENT_3 = new ItemBase("itemFluidComponent3", "fluid_component_3");
 }
