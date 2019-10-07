accafb4646229f3501764a1769384a0e668023de
Relentless <damnrelentless.yt@gmail.com>
2019-10-07 10:41:35 +0200

    Updated registry handler to support blocks and fluids

--- src/main/java/com/rlnt/ihmam/util/handlers/RegistryHandler.java
+++ src/main/java/com/rlnt/ihmam/util/handlers/RegistryHandler.java
@@ -1,11 +1,15 @@
 package com.rlnt.ihmam.util.handlers;
 
+import com.rlnt.ihmam.init.ModBlocks;
+import com.rlnt.ihmam.init.ModFluids;
 import com.rlnt.ihmam.init.ModItems;
 import com.rlnt.ihmam.util.interfaces.IHasModel;
+import net.minecraft.block.Block;
 import net.minecraft.item.Item;
 import net.minecraftforge.client.event.ModelRegistryEvent;
 import net.minecraftforge.event.RegistryEvent;
 import net.minecraftforge.fml.common.Mod.EventBusSubscriber;
+import net.minecraftforge.fml.common.event.FMLPreInitializationEvent;
 import net.minecraftforge.fml.common.eventhandler.SubscribeEvent;
 
 @EventBusSubscriber
@@ -17,11 +21,27 @@ public class RegistryHandler {
     }
 
     @SubscribeEvent
+    public static void registerBlocks(RegistryEvent.Register<Block> event) {
+        event.getRegistry().registerAll(ModBlocks.BLOCKS.toArray(new Block[0]));
+    }
+
+    @SubscribeEvent
     public static void registerModels(ModelRegistryEvent event) {
-        for(Item item : ModItems.ITEMS) {
+        for (Item item : ModItems.ITEMS) {
             if (item instanceof IHasModel) {
                 ((IHasModel)item).registerModels();
             }
         }
+
+        for (Block block : ModBlocks.BLOCKS) {
+            if (block instanceof IHasModel) {
+                ((IHasModel)block).registerModels();
+            }
+        }
+    }
+
+    public static void preInitRegistries(FMLPreInitializationEvent event) {
+        ModFluids.registerFluids();
+        RenderHandler.registerCustomMeshesAndStates();
     }
 }
